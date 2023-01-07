e122df49528d899bba9eb826b79f07b4a167788d
Isaac Halvorson <hello@hisaac.net>
2023-01-06 23:50:08 -0600

    Render some junk to a page

--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,38 @@ import { parse } from "flags";
 import slugify from "slugify";
 import MarkdownIt from "markdown-it";
 import { walkSync } from "fs";
-
-const postTextBundles: TextBundle[] = [];
-const postsDir = "site/blog";
-const postsWalker = walkSync(postsDir);
-for (const post of postsWalker) {
-	if (post.path.endsWith(".textbundle")) {
-		postTextBundles.push(TextBundle.fromPath(post.path));
-	}
-}
-
-console.log(postTextBundles);
+import { renderFile, configure } from "eta";
 
 const parsedArgs = parse(Deno.args);
 
 await Deno.remove("dist", { recursive: true });
 await Deno.mkdir("dist", { recursive: true });
+
+// Configure Eta
+configure({
+	views: "site/templates",
+})
+
+const postsDir = "site/blog";
+const postsWalker = walkSync(postsDir);
+
+for (const post of postsWalker) {
+	if (post.path.endsWith(".textbundle")) {
+		// postTextBundles.push(TextBundle.fromPath(post.path));
+		const bundle = TextBundle.fromPath(post.path);
+		const postHTML = renderFile("base.eta", {
+			title: bundle.info.title,
+			description: bundle.info.date,
+			content: bundle.text,
+		});
+
+		const postSlug = slugify(bundle.info.title);
+		const postPath = `./dist/${postSlug}.html`;
+
+		const postHTMLResolved = await Promise.resolve(postHTML);
+		if (postHTMLResolved) {
+			console.log(`Rendered ${postSlug}.html`);
+			Deno.writeTextFileSync(postPath, postHTMLResolved);
+		}
+	}
+}
