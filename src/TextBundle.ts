8c9bd4e51da511cb690fd801776aafb7b22ca5e2
Isaac Halvorson <hello@hisaac.net>
2023-01-07 13:52:17 -0600

    Add JSONValue type

--- src/TextBundle.ts
+++ src/TextBundle.ts
@@ -1,10 +1,18 @@
 import MarkdownIt from "markdown-it";
 
+// source: https://dev.to/ankittanna/how-to-create-a-type-for-complex-json-object-in-typescript-d81
+export type JSONValue =
+    | string
+    | number
+    | boolean
+    | { [x: string]: JSONValue }
+    | Array<JSONValue>;
+
 export class TextBundle {
-	info: any;
+	info: JSONValue;
 	text: string;
 
-	constructor(info: any, text: string) {
+	constructor(info: JSONValue, text: string) {
 		this.info = info;
 		this.text = text;
 	}
@@ -12,7 +20,7 @@ export class TextBundle {
 	static fromPath(path: string): TextBundle {
 		const decoder = new TextDecoder("utf-8");
 		const bundleText = decoder.decode(Deno.readFileSync(`${path}/text.md`));
-		const bundleInfo = JSON.parse(decoder.decode(Deno.readFileSync(`${path}/info.json`)));
+		const bundleInfo: JSONValue = JSON.parse(decoder.decode(Deno.readFileSync(`${path}/info.json`)));
 		return new TextBundle(bundleInfo, bundleText);
 	}
 
